5bb31798c063fee34b6bd3cc1a95ad4ac36ca4eb
M salman khan <mohammadsalmankhan3191@gmail.com>
2024-09-10 14:37:26 +0530

    operators in java

--- firstclass.java
+++ firstclass.java
@@ -5,10 +5,38 @@ class firstclass{
         try(Scanner SC=new Scanner(System.in)){
 
        
-        int a=SC.nextInt();
-        int b=SC.nextInt();
-        int sum=a+b;
-        System.out.println(sum);
+        // int a=SC.nextInt();
+        // int b=SC.nextInt();
+        int a=3,b=9;
+        // int sum=a+b;
+        // addidtion
+        System.out.println("a+b =" + (a+b));
+
+        // subtraction
+        System.out.println("a-b =" + (a-b));
+
+        // multiplication
+        System.out.println("a*b =" + (a*b));
+
+        // modulator operator
+        System.out.println("a%b =" + (a%b));
+
+        // divison
+        System.out.println("a/b =" + (a/b));
+        
+        // incement operator
+        System.out.println("a++ =" + (a++));
+
+        // += operator
+        System.out.println("a+=b =" + (a+=b));
+
+        //  -= operaotr
+        System.out.println("a-=b =" + (a-=b));
+
+        // decrement operator
+
+        System.out.println("a-- =" + (a--));
+
     }
     }
 }
